295d25beb22b65bae772298c21a0321f1b5b22f3
Muhammad Osama Khan <osammmakhan@gmail.com>
2025-01-20 09:17:11 +0500

    Removed secret.dart

--- lib/secret.dart
+++
@@ -1,6 +0,0 @@
-class Secret {
-  static const String apiKey = 'AIzaSyAypF89ApLUkusAtx1ersR1a4pn4x-xzgo';
-  static const String appId = '1:735235674573:android:21818f171c0113be5ff484';
-  static const String messagingSenderId = '735235674573';
-  static const String projectId = 'ezhealth-32082';
-}
